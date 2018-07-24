d42f95743bb6c192da653517fab3023fc0e32782
Devin Humphreys <humphred@advisory.com>
2018-07-24 12:14:50 -0400

    Added Blog app and homepage

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
